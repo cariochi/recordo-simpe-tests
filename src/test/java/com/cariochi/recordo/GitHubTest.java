224c421221306eb6dd398aa8cd175e9ffbeb36b8
vadimdeineka <vdeineka@star.global>
2020-09-10 16:21:54 +0300

    Remove @Verify annotation

--- src/test/java/com/cariochi/recordo/GitHubTest.java
+++ src/test/java/com/cariochi/recordo/GitHubTest.java
@@ -1,9 +1,9 @@
 package com.cariochi.recordo;
 
 
+import com.cariochi.recordo.given.Assertion;
 import com.cariochi.recordo.mockhttp.MockHttpContext;
 import com.cariochi.recordo.mockhttp.MockHttpServer;
-import com.cariochi.recordo.verify.Expected;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import feign.Feign;
@@ -34,32 +34,32 @@ class GitHubTest {
     @Test
     void test_given_verify(
             @Given("/dto/input.json") Dto dto,
-            @Verify("/dto/output.json") Expected<Dto> expected
+            @Given("/dto/output.json") Assertion<Dto> assertion
     ) {
         dto.setValue(dto.getValue() + "_updated");
-        expected.assertEquals(dto);
+        assertion.assertAsExpected(dto);
     }
 
     @Test
     @MockHttp("/http/gists.mock.json")
     void test_mock_http(
-            @Verify("/http/output.json") Expected<List<GitHub.GistResponse>> expected
+            @Given("/http/output.json") Assertion<List<GitHub.GistResponse>> responseAssertion
     ) {
         final List<GitHub.GistResponse> gists = github.getGists();
-        expected.assertEquals(gists);
+        responseAssertion.assertAsExpected(gists);
     }
 
     @Test
     void test_mock_http_with_variables(
             @MockHttp("/http/gists_with_variables.mock.json") MockHttpServer mockHttpServer,
-            @Verify("/http/output.json") Expected<List<GitHub.GistResponse>> expected
+            @Given("/http/output.json") Assertion<List<GitHub.GistResponse>> assertion
     ) {
         try (final MockHttpContext context = mockHttpServer.run()) {
             context.set("id1", "36387e79b940de553ad0b381afc29bf4");
             context.set("id2", "cc7e0f8678d69196387b623bd45f0f33");
             context.set("id3", "14c814e5561e8f03fce5f6d815af706c");
             final List<GitHub.GistResponse> gists = github.getGists();
-            expected.assertEquals(gists);
+            assertion.assertAsExpected(gists);
         }
     }
 
